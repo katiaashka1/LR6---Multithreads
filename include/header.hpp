cca1285e9a4a7c666aa35887e7cb92fa0b32e332
kelril <kelril@dsu1.service.sarnet>
2020-02-29 10:47:29 +0300

    0.1.6

--- include/header.hpp
+++ include/header.hpp
@@ -25,27 +25,29 @@ using std::endl;
 using std::vector;
 
 
-class hashMaster{
+class hashMaster {
 public:
-    hashMaster(){
+    uint64_t threadNumber;
+    vector <std::thread> allThreads;
+    
+    hashMaster() {
         threadNumber = std::thread::hardware_concurrency();
-        cout << "Thread count - " << threadNumber<<endl;
         setThreads();
     }
 
-    explicit hashMaster(int number){
+    explicit hashMaster(int number) {
         threadNumber = number;
         setThreads();
     }
 
 
 
-    void setThreads(){
-        for (uint64_t i = 0; i < threadNumber; i++){
+    void setThreads() {
+        for (uint64_t i = 0; i < threadNumber; i++) {
             std::thread th(hashHandler, i, this);
             allThreads.push_back(std::move(th));
         }
-        for (uint64_t i = 0; i < threadNumber; i++){
+        for (uint64_t i = 0; i < threadNumber; i++) {
             allThreads.at(i).join();
         }
     }
@@ -58,12 +60,12 @@ public:
             int stringLenth = 30;
             std::thread::id threadID=obj->allThreads.at(a).get_id();
             string stringForHash;
-            for (int i = 0; i < stringLenth; ++i){
+            for (int i = 0; i < stringLenth; ++i) {
                 stringForHash.push_back(std::rand()%255);
             }
             const std::string hash = picosha2::hash256_hex_string(stringForHash);
-            if (hash.find("0000", 60) == 60){
-                //cout<<"DONE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"<<endl;
+            if (hash.find("0000", 60) == 60) {
+                
                 BOOST_LOG_TRIVIAL(info) << stringForHash << " - " << hash << " - " << threadID << endl;
                 break;
             }
@@ -72,28 +74,21 @@ public:
         }
     }
 
-    static void init_logging()
-    {
+    static void init_logging() {
         logging::register_simple_formatter_factory<logging::trivial::severity_level, char > ("Severity");
 
-        logging::add_file_log
-                (
+        logging::add_file_log (
                         keywords::file_name = "info.log",
                         keywords::format = "[%TimeStamp%] [%ThreadID%] [%Severity%] %Message%"
                 );
 
-        logging::add_console_log(
+        logging::add_console_log (
                 std::cout,
                 keywords::format = "[%TimeStamp%] [%ThreadID%] [%Severity%] %Message%"
         );
 
         logging::add_common_attributes();
-
     }
-
-public:
-    uint64_t threadNumber;
-    vector <std::thread> allThreads;
 };
 
 #endif // INCLUDE_HEADER_HPP_
